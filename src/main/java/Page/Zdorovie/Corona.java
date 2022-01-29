5a50632699a87f240f77c00bb4fbabb5a7bd1bab
Ktulxy <omudritky@gmail.com>
2022-01-29 19:56:23 +0200

    Дошли до заполнения договора о покупке

--- src/main/java/Page/Zdorovie/Corona.java
+++ src/main/java/Page/Zdorovie/Corona.java
@@ -1,25 +1,33 @@
 package Page.Zdorovie;
 
 import Driver.BrowseDriver;
+import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static Driver.BrowseDriver.driver;
 
 public class Corona {
 
+    public static  WebElement birthDay = driver.findElement(By.xpath("//input[@id='7319SubjectInsuredBirthDate']"));
 
-    public class Coronavirus extends BrowseDriver {
+    public static  WebElement order = (new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='sumContainer']/input"))));
+   // public static WebElement order = driver.findElement(By.xpath("//div[@id='sumContainer']/input"));
 
-        public static WebElement coronaСhoice = driver.findElement(By.xpath("//div[@id='a1609846']/div/div/input[2]"));
+   public class Coronavirus extends BrowseDriver {
 
-
+         public static WebElement choice = driver.findElement(By.xpath("//div[@id='a1609846']/div/div/input[2]"));
 
     }
     public static void coronaData(String date){
+        birthDay.click();
+        birthDay.sendKeys(date + Keys.ENTER);
 
-        WebElement birthDay = driver.findElement(By.xpath("//input[@id='7319SubjectInsuredBirthDate']"));
-        birthDay.sendKeys(date);
 
     }
 }
--- src/test/java/Zdorovie/CoronavirusUA.java
+++ src/test/java/Zdorovie/CoronavirusUA.java
@@ -3,10 +3,13 @@ package Zdorovie;
 import Driver.BrowseDriver;
 import Page.Login;
 import Page.Zdorovie.Corona;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 import java.time.Duration;
 import static Page.Menu.Healthy;
-import static Page.Zdorovie.Corona.Coronavirus.coronaСhoice;
+import static Page.Zdorovie.Corona.*;
+import static Page.Zdorovie.Corona.Coronavirus.choice;
 
 
 public class CoronavirusUA extends BrowseDriver {
@@ -19,14 +22,21 @@ public class CoronavirusUA extends BrowseDriver {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         Healthy.click();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        coronaСhoice.click();
+        choice.click();
     }
 
     @Test(priority = 2)
     public void data(){
-
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         Corona.coronaData("01.01.2000");
-
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Actions actions = new Actions(driver);
+        actions.moveToElement(order);
+        actions.perform();
+        order.click();
     }
 }
